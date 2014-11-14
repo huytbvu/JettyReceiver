21a5b7e76e9c3f377382e752a8159bc8baef06fa
Huy Vu <huy.vu@ericsson.com>
2014-11-14 14:55:59 -0800

    update handler del

--- src/main/java/servlet/ListAppServlet.java
+++ src/main/java/servlet/ListAppServlet.java
@@ -11,7 +11,7 @@ import utils.CommandExecutor;
 
 public class ListAppServlet extends HttpServlet {
 	@Override
-	protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws IOException{
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException{
 		System.out.println("just receive a LIST");
 		BufferedReader br = req.getReader();
 		CommandExecutor.executeOperations("list", br,req.getPathInfo().substring(1));
