b795eec7c2bffd04105a038dffeb92707f4ccde7
Huy Vu <huy.vu@ericsson.com>
2014-11-25 09:56:00 -0800

    fix bug

--- src/main/java/apc/command/DockerCommand.java
+++ src/main/java/apc/command/DockerCommand.java
@@ -24,7 +24,7 @@ public class DockerCommand extends AbstractCommand{
 	public void addStartCmdParam(String envVar, String paramStartCmd){
 		String e = envVar == null ? "" : envVar;
 		cmdBuild.add("-s");
-		cmdBuild.add("sh -c 'echo "+e+" /etc/profile; "
+		cmdBuild.add("sh -c 'export "+e+"; "
 				+ "while true; do wget google.com; if [ $? -eq 0 ]; then break; fi; sleep 1; done; "
 				+ paramStartCmd +"' &");
 	}
